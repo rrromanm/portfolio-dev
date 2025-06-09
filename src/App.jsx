b7cfe28d6553069330842b376404d5170882d1cf
Romans Mihalonoks <147718413+rrromanm@users.noreply.github.com>
2025-06-10 00:47:38 +0200

    Fix router basename for GH Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio-dev">
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
