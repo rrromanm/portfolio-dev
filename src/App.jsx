fcf1e1ef8a2249826d03b6d9aadfecee1416f40f
Romans Mihalonoks <147718413+rrromanm@users.noreply.github.com>
2025-06-10 00:32:03 +0200

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css';
-import HomePage from './Pages/HomePage';
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
